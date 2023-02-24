4230f568b919abc424e2a94f3447efd13852dd54
Yashar A. Guseynov <yasharagu@code.local>
2023-02-24 13:26:24 +0400

    fibonaci task is created

---
+++ fibonaci1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fibonaci1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 0;
+            int secondNumber = 1;
+            int thirdNumber = 1;
+          
+            while (thirdNumber<9)
+            {
+                Console.WriteLine(thirdNumber);
+                thirdNumber = firstNumber + secondNumber;
+              firstNumber=secondNumber;
+                secondNumber = thirdNumber;
+                
+            }
+        }
+    }
+}
