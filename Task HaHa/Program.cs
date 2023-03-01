0f2cce01a67ffab0679a634c21a0745c738273f4
YasharGY <yasharagu@code.edu.az>
2023-03-01 21:34:30 +0400

    Task HaHa

---
+++ Task HaHa/Program.cs
@@ -0,0 +1,11 @@
+﻿  string Repeat(string word, int count)
+{
+    string repeatedString = "";
+    for (int i = 0; i < count; i++)
+    {
+        repeatedString += word;
+    }
+    return repeatedString;
+}
+string repeatedString = Repeat("Ha!", 3);
+Console.WriteLine(repeatedString);
