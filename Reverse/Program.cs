705668e88bb910dadd90dfd8e736da9c4f0c8cca
YasharGY <yasharagu@code.edu.az>
2023-03-06 00:20:48 +0400

    Reverse

---
+++ Reverse/Program.cs
@@ -0,0 +1,16 @@
+﻿ string CustomJoin(string[] arr, string separator)
+{
+    string result = string.Empty;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        result += arr[i];
+        if (i < arr.Length - 1)
+        {
+            result += separator;
+        }
+    }
+
+    return result;
+}
+Console.WriteLine();
