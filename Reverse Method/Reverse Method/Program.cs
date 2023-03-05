c94d5465e3d67a44cd7245b39dfb9a05b6c46a9c
YasharGY <yasharagu@code.edu.az>
2023-03-06 00:44:25 +0400

    Reverse Method

---
+++ Reverse Method/Reverse Method/Program.cs
@@ -0,0 +1,15 @@
+﻿public void ReverseArray(ref int[] arr)
+{
+    int temp;
+    int start = 0;
+    int end = arr.Length - 1;
+
+    while (start < end)
+    {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
